5e33ebf700a46bca55a11f8afab4fcaebb544daa
Matthew Fishkin <matt.fishkin@gmail.com>
2015-03-14 17:23:41 -0500

    XCODE project
    xcode project and all of the bugs :(

---
+++ Co-Llab/Co-Llab/CoLlab-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  CoLlab-Bridging-Header.h
+//  Co-Llab
+//
+//  Created by Matthew Fishkin on 3/14/15.
+//  Copyright (c) 2015 Fishkin. All rights reserved.
+//
+
+#ifndef Co_Llab_CoLlab_Bridging_Header_h
+#define Co_Llab_CoLlab_Bridging_Header_h
+
+#import "AFNetworking.h"
+
+#endif
---
+++ Co-Llab/Pods/Target Support Files/Pods-Co-Llab/Pods-Co-Llab-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Co_Llab : NSObject
+@end
+@implementation PodsDummy_Pods_Co_Llab
+@end
