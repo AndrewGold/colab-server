4a3b817c4c142cd4d38bdb44b53c095860b72a7c
Andrew Gold <andrewmichaelgold@gmail.com>
2015-03-14 17:50:57 -0500

    fixes

--- routes.js
+++ routes.js
@@ -2,6 +2,6 @@ var crypto = require('crypto');
 var express = require('express');
 module.exports = function(app) {
 	var users = require('./controllers/user_controller.js');
-	app.get('/signup', users.signup);
+	app.post('/signup', users.signup);
 	app.post('/login', users.login);
 }
